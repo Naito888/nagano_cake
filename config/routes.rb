5cff70b6029ed5f0d9778851a485c8cfc7f3f91b
EC2 Default User <ec2-user@ip-172-31-58-104.ec2.internal>
2022-07-10 07:20:44 +0000

    ルーティング記載変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,63 +1,50 @@
 Rails.application.routes.draw do
   
-  namespace :admin do
-    get 'orders/show'
-  end
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-  end
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/edit'
-  end
-  namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/show'
-    get 'items/edit'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :public do
-    get 'addresses/index'
-    get 'addresses/edit'
-  end
-  namespace :public do
-    get 'orders/new'
-    get 'orders/thanks'
-    get 'orders/index'
-    get 'orders/show'
-  end
-  namespace :public do
-    get 'cart_items/index'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/unsubscribe'
-  end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
-  # 顧客用
-  # URL /customers/sign_in ...
-  devise_for :customers,skip: [:passwords], controllers: {
-    registrations: "public/registrations",
-    sessions: 'public/sessions'
-  }
-  
   # 管理者用
   # URL /admin/sign_in ...
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
+  
+  namespace :admin do
+    root to: 'homes#top'
+    resources :products, except:[:destroy]
+    resources :genres, only:[:index, :edit, :create, :update]
+    resources :customers, only:[:index, :show, :edit, :update]
+    resources :orders, only:[:index, :show, :update]
+    resources :order_details, only:[:update]
+  end
+  
+  
+  # 顧客用
+  # URL /customers/sign_in ...
+  root to: "public/homes#top"
+  
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  
+  scope module: :public do
+    get "/about", to: "homes#about"
+    get "/customers/quit", to: "customers#quit"
+    patch "/customers/out", to: "customers#out"
+    resource :customers, only:[:show, :edit, :update]
+    resources :products, only:[:index, :show]
+    resources :cart_products, except:[:show, :new, :edit]
+    delete "/cart_products", to: "cart_products#destroy_all"
+    resources :orders, except:[:edit, :update, :destroy] do
+      collection do
+        get "complete"
+        post "check"
+      end
+    end
+    resources :deliveries, except:[:new, :show]
+    
+     # 検索機能
+    get '/genre_search', to: "searches#genre_search"
+    get '/product_search', to: "searches#product_search"
+  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
